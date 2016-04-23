5d880e4c6395535c75d57145534430e4e7491aae
mmiotti <marco@miotti.me>
2016-04-22 22:54:33 -0400

    Added files via upload

--- website/run.py
+++ website/run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def load_website():
-    return render_template('index.html')
+    return render_template('page.html')
 
 @app.route('/cars')
 def load_cars():
